c149d52090fe5301c0e9c2794ca7100b7efe4d51
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2023-06-04 18:51:22 +0100

    First Push

--- src/main/java/dev/zestyblaze/villagelife/entity/SmartVillagerMob.java
+++ src/main/java/dev/zestyblaze/villagelife/entity/SmartVillagerMob.java
@@ -10,7 +10,9 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.npc.VillagerData;
@@ -28,14 +30,15 @@ public class SmartVillagerMob extends PathfinderMob implements PolymerEntity {
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0d)
-                .add(Attributes.MOVEMENT_SPEED, 0.5d)
+                .add(Attributes.MOVEMENT_SPEED, 0.3d)
                 .add(Attributes.ATTACK_DAMAGE, 1.0d);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(0, new RandomStrollGoal(this, 1.2d));
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0d, false));
+        this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 1.0d, false));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0d));
+        this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
 
         this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, Animal.class, false));
     }
@@ -47,6 +50,8 @@ public class SmartVillagerMob extends PathfinderMob implements PolymerEntity {
 
     @Override
     public void modifyRawTrackedData(List<SynchedEntityData.DataValue<?>> data, ServerPlayer player, boolean initial) {
-        data.add(SynchedEntityData.DataValue.create(VillagerMixin.getVillagerData(), new VillagerData(VillagerType.JUNGLE, VillagerProfession.FARMER, 3)));
+        data.add(SynchedEntityData.DataValue.create(VillagerMixin.getVillagerData(),
+                new VillagerData(VillagerType.JUNGLE, VillagerProfession.BUTCHER, 5))
+        );
     }
 }
